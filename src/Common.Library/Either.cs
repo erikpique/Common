23e931b822df12ee487d4233bf2ecf05a64829f1
Erik Pique <epique@plainconcepts.com>
2022-07-30 22:30:27 +0200

    wip

--- src/Common.Library/Either.cs
+++ src/Common.Library/Either.cs
@@ -23,4 +23,33 @@ public abstract class Either<TLeft, TRight>
 
     public T Match<T>(Func<TLeft, T> left, Func<TRight, T> right) =>
         IsLeft ? left(_left) : right(_right);
+
+    public async Task<T> MatchAsync<T>(Func<TLeft, Task<T>> left, Func<TRight, Task<T>> right) =>
+        IsLeft ? await left(_left) : await right(_right);
+
+    public void Match(Action<TLeft> left, Action<TRight> right = default)
+    {
+        if (IsLeft)
+        {
+            left(_left);
+        }
+        else
+        {
+            right?.Invoke(_right);
+        }
+    }
+
+    public Task MatchAsync(Action<TLeft> left, Action<TRight> right = default)
+    {
+        if (IsLeft)
+        {
+            left(_left);
+        }
+        else
+        {
+            right?.Invoke(_right);
+        }
+
+        return Task.CompletedTask;
+    }
 }
